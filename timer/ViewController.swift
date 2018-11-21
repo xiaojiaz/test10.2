53561d91e3f951272e7c72db9925cf44dcd8f62b
student <student@imac-61.local>
2018-11-21 15:00:42 +0800

    Initial Commit

---
+++ timer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  timer
+//
+//  Created by student on 2018/11/21.
+//  Copyright © 2018年 123. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
