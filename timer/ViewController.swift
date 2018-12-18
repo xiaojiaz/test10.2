e09552d74c744096b3da644bf1c91db442ef2bfb
student <student@iMac-73.local>
2018-12-18 12:00:58 +0800

    first

--- timer/ViewController.swift
+++ timer/ViewController.swift
@@ -10,11 +10,53 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var myView: UIView!
+    @IBOutlet var frontview: UIView!
+    var timer:Timer?
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+//        timer=Timer.scheduledTimer(withTimeInterval: 1, repeats: true, block: {
+//            [weak weakSelf=self]
+//            (mytimer)in
+//            weakSelf?.myView.center.x += 20
+//            weakSelf?.myView.center.y += 20
+//        })
     }
 
 
+    @IBAction func btc(_ sender: Any) {
+        let myimage = UIImageView(frame: CGRect(x: 100, y: 100, width: 200, height: 200))
+        myimage.image = UIImage(named: "image")
+       UIView.transition(from: myView, to: myimage, duration: 2, options: .transitionFlipFromRight, completion: nil)
+//        UIView.transition(with: myView, duration: 2, options: .transitionCurlUp, animations: {
+//            self.myView.backgroundColor = UIColor.green
+//        }, completion: nil)
+//        timer?.invalidate()
+//        UIView.animate(withDuration: 4, delay: 1, options: [.curveLinear,.repeat,.autoreverse], animations: {
+//            self.myView.alpha = 0
+//            self.myView.center.x = self.view.bounds.width
+//            self.myView.backgroundColor = UIColor.green
+//            self.myView.transform = CGAffineTransform(rotationAngle: CGFloat(Double.pi))
+//            self.myView.transform = CGAffineTransform.identity
+//            self.myView.transform = CGAffineTransform(rotationAngle: CGFloat(Double.pi))
+//            self.myView.transform = CGAffineTransform.identity
+//            self.myView.transform = CGAffineTransform(rotationAngle: CGFloat(Double.pi))
+//            self.myView.transform = CGAffineTransform.identity
+//            self.myView.transform = CGAffineTransform(rotationAngle: CGFloat(Double.pi))
+//            self.myView.transform = CGAffineTransform.identity
+//            self.myView.transform = CGAffineTransform(rotationAngle: CGFloat(Double.pi))
+//            self.myView.transform = CGAffineTransform.identity
+//            self.myView.transform = CGAffineTransform(rotationAngle: CGFloat(Double.pi))
+//            self.myView.transform = CGAffineTransform.identity
+//            self.myView.transform = CGAffineTransform(rotationAngle: CGFloat(Double.pi))
+//            self.myView.transform = CGAffineTransform.identity
+//        }){
+//            (finished)in
+//            if finished{
+//                self.myView.removeFromSuperview()
+//            }
+//        }
+    }
 }
 
